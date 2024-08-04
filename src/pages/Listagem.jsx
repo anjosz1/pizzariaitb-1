a9e1ee2f8419aa3de4dee874ce70e3a554bb12d6
I4gojs <iagojosesobrinho65@gmail.com>
2024-08-03 22:49:36 -0300

    Final

--- src/pages/Listagem.jsx
+++ src/pages/Listagem.jsx
@@ -9,7 +9,7 @@ const Listagem = () => {
                 <img src={pizza.imagem}
                 alt={pizza.nome}
                 style={{
-                    width:"180",
+                    width:"150px",
                     height:"100px",
                     borderRadius:"8px",
                     margin:"0px",
