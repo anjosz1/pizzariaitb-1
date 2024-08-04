4f02919de2cb5549ce68ce5ae6b361b5ab5d12b1
I4gojs <iagojosesobrinho65@gmail.com>
2024-08-03 22:37:17 -0300

    erro

--- src/shared/pizzas.jsx
+++ src/shared/pizzas.jsx
@@ -1,43 +1,40 @@
+import frango from "../assets/image/FRANGO-COM-CATUPIRY.jpeg";
+import calabresa from "../assets/image/PIZZA-CALABRESA.jpeg";
+import mussarela from "../assets/image/PIZZA-MUSSARELA.jpeg";
+import bacon from "../assets/image/PIZZA-BACON.jpeg";
+import portuguesa from "../assets/image/PIZZA-PORTUGUESA.jpeg";
 
-import frango from "../assets/image/FRANGO-COM-CATUPIRY.jpg"
-import calabresa from "../assets/image/PIZZA-CALABRESA.jpeg"
-import mussarela from "../assets/image/PIZZA-MUSSARELA.jpeg"
-import bacon from "../assets/image/PIZZA-BACON.jpeg"
-import portuguesa from "../assets/image/PIZZA-PORTUGUESA.jpeg"
-const Pizzas =[
-
+const Pizzas = [
     {
         "id": 1,
-        "nome" : frango,
-        "descricao" : "Pizza de frango com catupiry",
+        "nome": "Frango com Catupiry",
+        "descricao": "Pizza de frango com catupiry",
         "imagem": frango
     },
     {
         "id": 2,
-        "nome" : calabresa,
-        "descricao" : "Pizza de calabresa",
+        "nome": "Calabresa",
+        "descricao": "Pizza de calabresa",
         "imagem": calabresa
     },
     {
         "id": 3,
-        "nome" : mussarela,
-        "descricao" : "Pizza de mussarela",
+        "nome": "Mussarela",
+        "descricao": "Pizza de mussarela",
         "imagem": mussarela
     },
     {
         "id": 4,
-        "nome" : bacon,
-        "descricao" : "Pizza de bacon",
+        "nome": "Bacon",
+        "descricao": "Pizza de bacon",
         "imagem": bacon
     },
     {
         "id": 5,
-        "nome" : portuguesa,
-        "descricao" : "Pizza portuguesa",
+        "nome": "Portuguesa",
+        "descricao": "Pizza portuguesa",
         "imagem": portuguesa
     },
-
-
-]
+];
 
 export default Pizzas;
